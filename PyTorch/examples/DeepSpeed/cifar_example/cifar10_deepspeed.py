5d5b124cf4d216c8d575d30f85501193815fc92a
mandy-li <mjli@habana.ai>
2022-12-13 15:13:27 -0800

    Fix patch to pillow version and DeepSpeed example

--- PyTorch/examples/DeepSpeed/cifar_example/cifar10_deepspeed.py
+++ PyTorch/examples/DeepSpeed/cifar_example/cifar10_deepspeed.py
@@ -166,7 +166,7 @@ def imshow(img):
 
 # get some random training images
 dataiter = iter(trainloader)
-images, labels = dataiter.next()
+images, labels = next(dataiter)
 
 # show images
 imshow(torchvision.utils.make_grid(images))
@@ -331,7 +331,7 @@ print('Finished Training')
 # Okay, first step. Let us display an image from the test set to get familiar.
 
 dataiter = iter(testloader)
-images, labels = dataiter.next()
+images, labels = next(dataiter)
 
 # print images
 imshow(torchvision.utils.make_grid(images))
